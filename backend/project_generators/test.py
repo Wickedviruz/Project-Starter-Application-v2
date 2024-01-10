b4e2ae72d33f8398dab1ec1a82a6bed8c1d2c4c3
Johan Ivarsson <86954289+Wickedviruz@users.noreply.github.com>
2024-01-10 20:51:55 +0100

    Delete backend/project_generators/test.py

--- backend/project_generators/test.py
+++
@@ -1,8 +0,0 @@
-import tkinter_project
-
-# Ersätt dessa sökvägar och namn med lämpliga testvärden
-test_path = r'C:\Users\johan\OneDrive\Dokument\Playground\test'
-test_name = 'test_project'
-
-# Testa funktionen direkt
-tkinter_project.create_tkinter_project(test_path, test_name)
